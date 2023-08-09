c69e11f6734a30606a195dd5f88e060d3918675c
prajwalh02 <99700885+prajwalh02@users.noreply.github.com>
2023-08-09 14:29:46 +0530

    Attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//      System.out.println(obj.name);       //java: name has private access in com.driver.RWOnly
+
+//        set a value to a name
+        obj.setName("Prajwal Hole");
+
+//        get a value
+        System.out.println(obj.getName());
+    }
 }
